fbb488ae0be111de041130d9e711b07081e137c0
Terenceonye <onyeweketerence@gmail.com>
2023-02-06 18:58:42 -0500

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
-'''fcreates and distributes an archive to your web servers, using deploy():
-'''
-
-import os
+"""
+Fabric script that distributes an archive to your web servers
+"""
+=======
+# Fabfile to create and distribute an archive to a web server.
+import os.path
 from datetime import datetime
-from fabric.api import env, local, put, run, runs_once
+from fabric.api import env
+from fabric.api import local
+from fabric.api import put
+from fabric.api import run
 
 
-env.hosts = ['54.157.144.59', '54.237.8.122']
+from datetime import datetime
+from fabric.api import *
+import os
+
+env.hosts = ["54.157.144.59", "54.237.8.122"]
 
 
-@runs_once
-def do_pack():
-    """Archives the static files."""
+def do_deploy(archive_path):
+    """Distributes an archive to a web server.
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
+    """
     if not os.path.isdir("versions"):
         os.mkdir("versions")
     cur_time = datetime.now()
@@ -54,15 +68,8 @@ def do_deploy(archive_path):
         run("rm -rf {}web_static".format(folder_path))
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(folder_path))
-        print('New version is now LIVE!')
+        print('New version deployed!')
         success = True
     except Exception:
         success = False
     return success
-
-
-def deploy():
-    """Archives and deploys the static files to the host servers.
-    """
-    archive_path = do_pack()
-    return do_deploy(archive_path) if archive_path else False
